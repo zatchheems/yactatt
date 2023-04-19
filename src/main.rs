2d03c38c051df72bed62a67d6258966e012f7ff8
Zak Hammerman <zak.hammerman@gmail.com>
2023-04-18 23:14:55 -0500

    Add basic bus predictions rendering(!)
    Also fixes weird color rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-use std::{time, thread, collections::{BTreeMap}};
+#![warn(missing_docs)]
+
+//! YACTATT: Yet Another CTA Transit Tracker
+//!
+//! This is a simple standalone embedded Rust program designed to take
+//! bus/train routes and stops/stations respectively and output them
+//! in order of arrival time on an RGB LED matrix.
+
+use std::{time::{self, SystemTime}, collections::{BTreeMap}};
 use chrono::NaiveDateTime;
 use clap::{arg, Command};
 use reqwest::{self, StatusCode, Url, header::ACCEPT};
 use rpi_led_panel::{RGBMatrix, RGBMatrixConfig, Canvas};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize};
 use embedded_graphics::{
     primitives::{Rectangle, Primitive, PrimitiveStyle, Line},
     text::Text,
-    mono_font::{ascii::FONT_9X15_BOLD, MonoTextStyle},
+    mono_font::{ascii::{FONT_9X15_BOLD, FONT_4X6}, MonoTextStyle},
     prelude::*,
     pixelcolor::Rgb888,
     Drawable
 };
-
-struct URL {
-    protocol: String,
-    domain: String,
-    path: String,
-    query: String
-}
 
 const BUSTRACKER_URL: &str =
     "http://www.ctabustracker.com/bustime/api/v2/getpredictions?format=json&key=<KEY>&rt=50&stpid=1802";
@@ -26,10 +27,12 @@ const BUSTRACKER_URL: &str =
 const TRAINTRACKER_URL: &str =
     "http://lapi.transitchicago.com/api/1.0/totarrivals.aspxoutputType=JSON&key=<KEY>&mapid=40380";
 
+const VERTICAL_OFFSET: i32 = 6;
+
 // use reqwest::header::Authorization;
 // use reqwest::header::ContentType;
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 enum Prediction {
     A,
     D
@@ -37,7 +40,7 @@ enum Prediction {
 
 // TODO: use #[serde(flatten)] to remove needless nested keys
 
-#[derive(Deserialize, Debug)]
+#[derive(Clone, Deserialize, Debug)]
 struct CTABus {
     tmstmp: String, // "YYYYMMDD HH24:MI"
     typ: Prediction,
@@ -50,18 +53,15 @@ struct CTABus {
     rtdir: String, // Direction (Southbound, etc.)
     des: String, // Destination
     prdtm: String, // Predicted time to arrival
+    #[serde(skip)]
     tablockid: String, // TA block ID
+    #[serde(skip)]
     tatripid: String, // TA trip ID
     origtatripno: String,
     dly: bool, // Delayed?
     prdctdn: String, // Time, in minutes, until bus arrives
+    #[serde(skip)]
     zone: String // Empty string if vehicle has not entered a defined zone
-}
-
-impl CTABus {
-    fn draw(&self, mut matrix: RGBMatrix, mut canvas: Box<Canvas>){
-        
-    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -71,8 +71,21 @@ struct CTABusTimesResponse {
 }
 
 #[derive(Deserialize, Debug)]
+struct CTABusError {
+    rt: String,
+    stpid: String,
+    msg: String
+}
+
+type CTABusErrors = Vec<CTABusError>;
+
+/// Bus predictions can either have the key "prd" for predictions or "error" if something went wrong.
+#[derive(Deserialize, Debug)]
 struct CTABusTimesPredictions {
-    prd: CTABusTimes
+    #[serde(default)]
+    prd: CTABusTimes,
+    #[serde(default)]
+    error: CTABusErrors
 }
 
 type CTABusTimes = Vec<CTABus>;
@@ -94,9 +107,21 @@ enum CTAVehicle {
     Train(CTATrain)
 }
 
+/// CTAVehicle structs can draw their own data given a matrix and a canvas.
+/// This lets us loop over each struct we deserialize after calling the CTA API.
 impl CTAVehicle {
-    fn draw() {
-        println!("TODO: Draw function on struct CTAVehicle. Takes reference to LED matrix")
+    fn draw(&self, canvas: &mut Box<Canvas>, x: i32, y: i32) {
+        let (route, prediction, destination) = match self {
+            CTAVehicle::Bus(bus) => (String::clone(&bus.rt), String::clone(&bus.prdctdn), String::clone(&bus.des)),
+            CTAVehicle::Train(train) => (String::from("red"), String::from("??"), String::from("Hell!"))
+        };
+        let cta_sign_grey: i32 = 0x565a5c;
+        Text::new(&route, Point::new(x,y), MonoTextStyle::new(&FONT_4X6, i32_to_rgb888(0xFFA600)))
+            .draw(canvas.as_mut()).unwrap();
+        Text::new(&prediction, Point::new(x + 10,y), MonoTextStyle::new(&FONT_4X6, i32_to_rgb888(cta_sign_grey)))
+            .draw(canvas.as_mut()).unwrap();
+        Text::new(&destination, Point::new(x + 20,y), MonoTextStyle::new(&FONT_4X6, i32_to_rgb888(cta_sign_grey)))
+            .draw(canvas.as_mut()).unwrap();
     }
 }
 
@@ -107,53 +132,83 @@ async fn main() {
         .author("Zak Hammerman <@zatchheems>")
         .about("Yet Another CTA Transit Tracker")
         .args([
+            arg!(-b --brightness <u8> "LED screen brightness").value_parser(clap::value_parser!(u8)),
             arg!(-r --rows <usize> "LED matrix rows").value_parser(clap::value_parser!(usize)),
             arg!(-c --cols <usize> "LED matrix columns").value_parser(clap::value_parser!(usize)),
             arg!(-R --refresh_rate <usize> "Refresh rate").value_parser(clap::value_parser!(usize)),
             // TODO: use existence as implicit true/false
             arg!(-s --silent <bool> "Silent (no LED display)").value_parser(clap::value_parser!(bool)),
+            arg!(-t --timeout <f32> "Time between refetch").value_parser(clap::value_parser!(f32)),
         ])
         .get_matches();
 
-
+    let brightness: u8 = *arguments.get_one::<u8>("brightness").unwrap_or(&7);
     let rows: usize = *arguments.get_one::<usize>("rows").unwrap_or(&16);
     let cols: usize = *arguments.get_one::<usize>("cols").unwrap_or(&64);
     let refresh: usize = *arguments.get_one::<usize>("refresh_rate").unwrap_or(&120);
     let silent: bool = *arguments.get_one::<bool>("silent").unwrap_or(&false);
-    let (mut matrix, mut canvas);
+    let client = reqwest::Client::new();
+    let timeout = *arguments.get_one::<f32>("timeout").unwrap_or(&60.0);
 
-    // Set up LED panel framework
+    // Predeclare matrix and canvas for use later. Not initialized by default since
+    // we may want to run it in "silent" mode, wherein no LED panel is used. Great for debugging.
+    let (matrix, mut canvas): (RGBMatrix, Box<Canvas>);
+
     println!("Starting YACTATT...");
 
+    // If not using the silent debug mode, set up LED panel framework
     if !silent {
         (matrix, canvas) = initialize_matrix(rows, cols, refresh);
-        splash_screen(rows, cols, refresh, matrix, canvas);
+        let _ = &canvas.set_brightness(brightness);
+        let (matrix_, _) = initialize_matrix(rows, cols, refresh);
+        splash_screen(32, 64, matrix, canvas.clone());
+        begin_tracker_loop(&client, time::Duration::from_secs_f32(timeout), silent, matrix_, canvas).await;
     }
-    
-    let client = reqwest::Client::new();
-    
-    // TODO: build args from env::args()
-    let refresh_rate = time::Duration::from_secs_f32(60.0);
-
-    begin_tracker_loop(&client, refresh_rate).await;
 
     println!("Exiting... hope you caught a ride!");
 }
 
-// Main loop. Handles refreshing LED panel and fetching & parsing tracking data.
-async fn begin_tracker_loop(client: &reqwest::Client, refresh_rate: time::Duration) -> () {
+/// Main loop. Handles refreshing LED panel and fetching & parsing tracking data.
+async fn begin_tracker_loop(
+    client: &reqwest::Client,
+    timeout: time::Duration,
+    silent: bool,
+    mut matrix: RGBMatrix,
+    mut canvas: Box<Canvas>,
+) -> () {
     loop {
+        let now = SystemTime::now();
         match cta_api_request(&client, BUSTRACKER_URL).await {
-            Some(buses) => println!("{:?}", buses),
+            Some(buses) => {
+                println!("{:?}", buses);
+                if !silent {
+                    loop {
+                        let mut offset = VERTICAL_OFFSET;
+                        // FIXME: crashes when prd is empty. Check .errors
+                        buses.prd
+                            .clone()
+                            .into_iter()
+                            .for_each(|bus| {
+                                    CTAVehicle::draw(&CTAVehicle::Bus(bus), &mut canvas, 1, offset);
+                                    offset += VERTICAL_OFFSET;
+                                });
+                        canvas = matrix.update_on_vsync(canvas);
+
+                        if now.elapsed().unwrap() > timeout {
+                            canvas.clear(Rgb888::new(0,0,0));
+                            break;
+                        }
+                    }
+                }
+            },
             None => println!("Missing bus response..."),
         }
-        thread::sleep(refresh_rate);
     }
 }
 
-// Make an async request to the CTA transit tracker API of choice.
+/// Make an async request to the CTA transit tracker API of choice.
 // FIXME: properly handle the generic arg.
-async fn cta_api_request(client: &reqwest::Client, api_url: &str ) -> Option<CTABusTimes> {
+async fn cta_api_request(client: &reqwest::Client, api_url: &str ) -> Option<CTABusTimesPredictions> {
     let url = Url::parse(api_url).expect("Invalid URL given.");
     let response =
         client.get(url)
@@ -165,9 +220,20 @@ async fn cta_api_request(client: &reqwest::Client, api_url: &str ) -> Option<CTA
     // TODO: map buses/trains to vector of structs
     match response.status() {
         StatusCode::OK => {
-            // println!("{:?}", response.text().await);
+            // FIXME: no prd key exists when no service is scheduled:
+            /* {
+                "bustime-response": {
+                    "error": [
+                        {
+                            "rt": "50",
+                            "stpid": "1802",
+                            "msg": "No service scheduled"
+                        }
+                    ]
+                }
+            }*/
             let bustimes = response.json::<CTABusTimesResponse>().await.unwrap();
-            Some(bustimes.bustime_response.prd)
+            Some(bustimes.bustime_response)
         },//response.json::<Vec<CTATrain>>,//Some(vec![result]),
         // Err(reqwest::StatusCode::NOT_FOUND) => println!("404 Not found"),
         // Err(reqwest::StatusCode::BAD_GATEWAY) => println!("502 Bad gateway"),
@@ -178,6 +244,7 @@ async fn cta_api_request(client: &reqwest::Client, api_url: &str ) -> Option<CTA
     // return body;
 }
 
+/// Create a new RGBMatrix and boxed Canvas.
 fn initialize_matrix(rows: usize, cols: usize, refresh_rate: usize) -> (RGBMatrix, Box<Canvas>) {
 // TODO: add command-line args for all required struct fields
     let config: RGBMatrixConfig = RGBMatrixConfig{
@@ -196,17 +263,31 @@ fn initialize_matrix(rows: usize, cols: usize, refresh_rate: usize) -> (RGBMatri
     RGBMatrix::new(config, 0).expect("Failed to initialize matrix.")
 }
 
-
+/// Convert 32-bit integers to Rgb888 structs for use in embedded graphics.
+/// 
+/// Examples:
+/// 
+/// ```rust
+/// let coffee = i32_to_rgb888(#c0ffee);
+/// assert_eq!(coffee, RGB888::new(192, 255, 254));
+/// ```
 fn i32_to_rgb888(color: i32) -> Rgb888 {
     // Split out color components from hex codes
     let r: u8 = ((color & 0xff0000) >> 16) as u8;
-    let g: u8 = ((color & 0x00ff00) >> 8) as u8;
-    let b: u8 = (color & 0x0000ff) as u8;
+    let g: u8 = (color & 0x0000ff) as u8;
+    let b: u8 = ((color & 0x00ff00) >> 8) as u8;
     Rgb888::new(r, g, b)
 
 }
 
-fn splash_screen(rows: usize, cols: usize, refresh_rate: usize, mut matrix: RGBMatrix, mut canvas: Box<Canvas>) {
+/// Draw a quick, simple test to show all CTA colors.
+/// TODO:
+///     - show version, device info, my handle, etc.
+///     - cool animations, because why not
+///     - different alternate screens:
+///         + smooth gradient between colors
+///         + concentric outlined boxes that build in, show text, and blink out consecutively
+fn splash_screen(rows: usize, cols: usize, mut matrix: RGBMatrix, mut canvas: Box<Canvas>) {
     // TODO: static, constant CTA colors
     let cta_train_colors: BTreeMap<&str, i32> = BTreeMap::from([
         ("red", 0xc60c30),
@@ -220,9 +301,11 @@ fn splash_screen(rows: usize, cols: usize, refresh_rate: usize, mut matrix: RGBM
     ]);
     let cta_sign_grey: i32 = 0x565a5c;
     let mut offset: i32 = 0;
+    // Record start time so we can break after showing splash screen for several seconds
+    let now = SystemTime::now();
     // Draw splash screen
-    for _step in 0.. {
-        for (name, color) in &cta_train_colors {
+    loop {
+        for (_name, color) in &cta_train_colors {
             let top_left: Point = Point::new(
                 (offset % 4) * (cols as i32 / 4),
                 if offset > 4 {rows as i32 / 2} else {0}
@@ -250,7 +333,9 @@ fn splash_screen(rows: usize, cols: usize, refresh_rate: usize, mut matrix: RGBM
         // TODO: print version number and my handle
         // FIXME: HORRIBLE flicker
         canvas = matrix.update_on_vsync(canvas);
-        // thread::sleep(std::time::Duration::from_secs_f32(5.0));
         offset = 0;
+        if now.elapsed().unwrap().as_secs() > 5 {
+            break;
+        }
     }
 }
